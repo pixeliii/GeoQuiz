2977a99674d0ae6dfc0499cc4d7b5d5ccbf339f1
pixeliii <axel.andersin@gmail.com>
2014-10-15 23:16:47 +0300

    Initial commit

---
+++ app/src/main/java/com/pixeli/geoquiz/TrueFalse.java
@@ -0,0 +1,34 @@
+package com.pixeli.geoquiz;
+
+
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTruequestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTruequestion = trueQuestion;
+    }
+
+    public void getQuestion() {
+
+    }
+
+    public void setQuestion(int question) {
+
+
+    }
+
+    public void isTruequestion() {
+
+    }
+
+    public void setTruequestion(boolean trueQuestion) {
+        mTruequestion = trueQuestion;
+    }
+
+
+
+
+    }
